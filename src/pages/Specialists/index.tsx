f222f54bfeb784c68dbfa1b7c820c0ca5b04c5dc
Grigor <artgrigapp@gmail.com>
2023-12-21 10:21:42 +0400

    Configure eslint and prettier

--- src/pages/Specialists/index.tsx
+++ src/pages/Specialists/index.tsx
@@ -1,11 +1,7 @@
-import {FC} from "react";
+import { FC } from 'react'
 
-const Specialists:FC = () => {
-    return (
-        <div>
-            Specialists page
-        </div>
-    );
-};
+const Specialists: FC = () => {
+  return <div>Specialists page</div>
+}
 
-export default Specialists;
+export default Specialists
